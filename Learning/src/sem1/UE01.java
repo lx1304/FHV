2ba61cef92f0e773ab13b67ed1c9e1de514a7e4a
Alex Wagner <lx1304-github@yahoo.de>
2014-12-22 21:02:33 +0100

    First test

---
+++ Learning/src/sem1/UE01.java
@@ -0,0 +1,21 @@
+/* UE01.java
+ * 
+ * author Alexander Wagner
+ * date 22.12.2014
+ *
+ * class names
+ */
+
+package sem1;
+
+/**
+ * Class description goes here.
+ *
+ * @date 22.12.2014
+ * @author Alexander Wagner
+ *
+ */
+
+public class UE01 {
+
+}
